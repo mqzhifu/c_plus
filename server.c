eebedc3a7784b5cd07321470652f5d27f6cf0f1f
mqzhifu <mqzhifu@sina.com>
2023-07-24 18:11:07 +0800

    ddd

--- server.c
+++ server.c
@@ -89,16 +89,12 @@ int send_data(int socket, char *s) {
     if (result == -1) {
         fprintf(stderr, "%s: %s \n","和客户端通信发生错误",strerror(errno));
     }
+
+    printf("send_data:%s \n",s);
     return result;
 }
 
-
-
-void main(){
-    //调用socket函数返回的文件描述符
-	int     serverSocket;
-    //声明两个套接字 sockaddr_in 结构体变量，分别表示客户端和服务器
-    struct  sockaddr_in server_addr;
+void accept_client(int serverSocket ){
     struct  sockaddr_in clientAddr;//accept 中返回的参数
     int     addr_len = sizeof(clientAddr);
     int     client;//clientAddr 对应的 socketFD Id
@@ -108,7 +104,80 @@ void main(){
     //计数器，无用
     int     recvBuffDataCnt = 0;
 
+    //调用accept，会进入阻塞状态,accept返回一个套接字FD，便有两个FD:serverSocket和client
+    //serverSocket仍然继续在监听状态，client则负责接收和发送数据
 
+    //clientAddr 是一个传出参数，accept返回时，传出客户端的地址和端口号
+    //addr_len 是一个传入 传出参数，传入的是调用者提供的缓冲区的 clientAddr 的长度，以避免缓冲区溢出。
+    //传出的是客户端地址结构体的实际长度。
+    //出错返回-1
+
+    client = accept(serverSocket, (struct sockaddr*)&clientAddr, (socklen_t*)&addr_len);
+    if(client < 0){
+        error("accept",-5);
+        continue;
+    }
+
+    printf("accept client:%d.\n",client);
+
+
+    //inet_ntoa   ip地址转换函数，将网络字节序IP转换为点分十进制IP
+    //表达式：char *inet_ntoa (struct in_addr);
+    printf("IP is %s\n", inet_ntoa(clientAddr.sin_addr));
+    printf("Port is %d\n", htons(clientAddr.sin_port));
+
+    //一但 accept 函数成功 返回一个 client socketFD，就要立刻做 SOCKET IO 处理，要创建一个新的进程，避免阻塞
+    pid_t pid;
+    pid = fork();
+    printf("fork new process:\n");
+    if(pid < 0){
+        error("fork error",-9);
+    }else if(pid == 0){
+        printf("child process,start recv data......\n");
+        //接收数据缓冲区
+//            char final_recv_data[255];
+//            while(1){//这里不能循环，recv 会造成阻塞
+            recvDataLen = recv(client, recvDataBuffer, sizeof(recvDataBuffer), 0);//阻塞接收客户端的数据
+            printf("recvDataLen:%d \n",recvDataLen);
+            if(recvDataLen < 0)
+            {
+                error("recv error",-6);
+//                    break;
+            }
+
+            if(recvDataLen == 0){
+                printf("recv client data end.\n");
+                error("recvDataLen == 0",-7);
+//                    break;
+            }
+
+            if(recvDataLen > recvDataBufferMax ){//客户端一次发送过来的数据，大于 buffer
+                error("recvDataLen > recvDataBufferMax",-8);
+            }
+//                printf("strcat data \n");
+//                strcat(final_recv_data,buffer);
+//                //这里防止死循环，也是防止C端恶意攻击
+//                recvBuffDataCnt++;
+//                if(recvBuffDataCnt > 10){
+//                    printf(" err,cnt > 10 exec!\n");
+//                    break;
+//                }
+//            }
+            printf("recv  data:%s \n",recvDataBuffer);
+        //printf("recv_str_num:%d,recv data is: %s,send_data:%s\n", strlen(final_recv_data), final_recv_data,"yes!");
+        char send_data_arr[] = "yes,im z!";
+        send_data(client,send_data_arr);
+    }else{
+        printf("fork , im father ,nothing to do...\n");
+        //父进程，不做任何操作，返回
+    }
+}
+
+void main(){
+    //调用socket函数返回的文件描述符
+	int     serverSocket;
+    //声明两个套接字 sockaddr_in 结构体变量，分别表示客户端和服务器
+//    struct  sockaddr_in server_addr;
     //创建socket
     serverSocket = open_listener_socket();
 
@@ -130,76 +199,7 @@ void main(){
     printf("Listening on port: %d\n", SERVER_PORT);
 
     while(1){
-        //调用accept，会进入阻塞状态,accept返回一个套接字FD，便有两个FD:serverSocket和client
-        //serverSocket仍然继续在监听状态，client则负责接收和发送数据
-
-        //clientAddr 是一个传出参数，accept返回时，传出客户端的地址和端口号
-        //addr_len 是一个传入 传出参数，传入的是调用者提供的缓冲区的 clientAddr 的长度，以避免缓冲区溢出。
-        //传出的是客户端地址结构体的实际长度。
-        //出错返回-1
-
-        client = accept(serverSocket, (struct sockaddr*)&clientAddr, (socklen_t*)&addr_len);
-        if(client < 0){
-            error("accept",-5);
-            continue;
-        }
-
-        printf("accept client:%d.\n",client);
-
-
-        //inet_ntoa   ip地址转换函数，将网络字节序IP转换为点分十进制IP
-        //表达式：char *inet_ntoa (struct in_addr);
-        printf("IP is %s\n", inet_ntoa(clientAddr.sin_addr));
-        printf("Port is %d\n", htons(clientAddr.sin_port));
-
-        //一但 accept 函数成功 返回一个 client socketFD，就要立刻做 SOCKET IO 处理，要创建一个新的进程，避免阻塞
-        pid_t pid;
-        pid = fork();
-        printf("fork new process:\n");
-        if(pid < 0){
-            error("fork error",-9);
-        }else if(pid == 0){
-            printf("child process,start recv data......\n");
-            //接收数据缓冲区
-//            char final_recv_data[255];
-//            while(1){//这里不能循环，recv 会造成阻塞
-                recvDataLen = recv(client, recvDataBuffer, sizeof(recvDataBuffer), 0);//阻塞接收客户端的数据
-                printf("recvDataLen:%d \n",recvDataLen);
-                if(recvDataLen < 0)
-                {
-                    error("recv error",-6);
-//                    break;
-                }
-
-                if(recvDataLen == 0){
-                    printf("recv client data end.\n");
-                    error("recvDataLen == 0",-7);
-//                    break;
-                }
-
-                if(recvDataLen > recvDataBufferMax ){//客户端一次发送过来的数据，大于 buffer
-                    error("recvDataLen > recvDataBufferMax",-8);
-                }
-//                printf("strcat data \n");
-//                strcat(final_recv_data,buffer);
-//                //这里防止死循环，也是防止C端恶意攻击
-//                recvBuffDataCnt++;
-//                if(recvBuffDataCnt > 10){
-//                    printf(" err,cnt > 10 exec!\n");
-//                    break;
-//                }
-//            }
-                printf("recv  data:%s \n",recvDataBuffer);
-
-
-            //printf("recv_str_num:%d,recv data is: %s,send_data:%s\n", strlen(final_recv_data), final_recv_data,"yes!");
-            char send_data_arr[] = "yes,im z!";
-
-            send_data(client,send_data_arr);
-        }else{
-            printf("fork , im father ,nothing to do...\n");
-            //父进程，不做任何操作，返回
-        }
+        accept_client(serverSocket);
     }
 
 }
